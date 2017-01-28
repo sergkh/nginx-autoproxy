85c025f6f1c8d24bfb05d88af992df9b5066871c
sergkh <sergey.khruschak@gmail.com>
2017-01-28 23:30:55 +0200

    typo fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nohup sh -c "confd -backend ${STORAGE_BACKEND:-etcd}ś -node ${STORAGE_NODE:-"http://etcd:2379"}" &
+nohup sh -c "confd -backend ${STORAGE_BACKEND:-etcd} -node ${STORAGE_NODE:-"http://etcd:2379"}" &
 nginx -g "daemon off;"
